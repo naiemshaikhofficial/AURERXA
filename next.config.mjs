fe527cbacc0d8354414508cdc22b34f7b0f04646
naiemshaikhofficial <naiemshaikhofficial@gmail.com>
2026-02-07 12:53:44 +0530

    config: Add Supabase image domain to Next.js remote patterns.

--- next.config.mjs
+++ next.config.mjs
@@ -41,6 +41,10 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'm.media-amazon.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'xquczexikijzbzcuvmqh.supabase.co',
       }
     ],
   },
