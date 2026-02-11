9729287639bb91bffbd2242fb5d0b2a79b442020
naiemshaikhofficial <naiemshaikhofficial@gmail.com>
2026-02-11 21:24:55 +0530

    feat: add new image sizes to Next.js configuration and update development type import path.

--- next.config.mjs
+++ next.config.mjs
@@ -64,7 +64,7 @@ const nextConfig = {
     dangerouslyAllowSVG: true,
     minimumCacheTTL: 31536000, // Cache optimized images for 1 year
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 480, 576],
     remotePatterns: [
       {
         protocol: 'https',
