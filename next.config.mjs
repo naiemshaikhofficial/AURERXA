7ab3e7c18c2149d84a79bccc770e57ff0ec86d99
naiemshaikhofficial <naiemshaikhofficial@gmail.com>
2026-02-19 10:45:42 +0530

    feat: Add `web.telegram.org` to the image source Content Security Policy and remote patterns.

--- next.config.mjs
+++ next.config.mjs
@@ -48,7 +48,7 @@ const nextConfig = {
               "default-src 'self'",
               "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn.jsdelivr.net https://checkout.razorpay.com https://*.cashfree.com",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-              "img-src 'self' blob: data: https://*.supabase.co https://img.icons8.com https://images.pexels.com https://images.unsplash.com https://encrypted-tbn0.gstatic.com https://encrypted-tbn1.gstatic.com https://encrypted-tbn2.gstatic.com https://encrypted-tbn3.gstatic.com https://m.media-amazon.com https://*.razorpay.com https://img.youtube.com https://imagizer.imageshack.com https://imageshack.com *",
+              "img-src 'self' blob: data: https://*.supabase.co https://img.icons8.com https://images.pexels.com https://images.unsplash.com https://encrypted-tbn0.gstatic.com https://encrypted-tbn1.gstatic.com https://encrypted-tbn2.gstatic.com https://encrypted-tbn3.gstatic.com https://m.media-amazon.com https://*.razorpay.com https://img.youtube.com https://imagizer.imageshack.com https://imageshack.com https://web.telegram.org *",
               "font-src 'self' https://fonts.gstatic.com data:",
               "connect-src 'self' https://*.supabase.co https://cdn.jsdelivr.net https://*.razorpay.com https://*.cashfree.com",
               "media-src 'self' blob: data: https://*.supabase.co",
@@ -130,6 +130,10 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'imageshack.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'web.telegram.org',
       }
     ],
   },
